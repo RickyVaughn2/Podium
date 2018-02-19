7151fd93bbca17247f5d487a6c1f839a3205a931
Ricky Vaughn <rickyvaughn2@gmail.com>
2018-02-19 16:08:19 -0600

    get_reps is used to pull reps by address
    get_reps is used to pull reps by address using the google civic API

---
+++ get_reps.py
@@ -0,0 +1,16 @@
+import requests
+import argparse
+
+# Input arg (should be user to scrape)
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', '--key', help='google api key', required=True)
+parser.add_argument('-a', '--address', help='address to search', required=True)
+args = parser.parse_args()
+
+query = {'key': args.key, 'address': args.address}
+url = 'https://www.googleapis.com/civicinfo/v2/representatives'
+req = requests.get(url, params=query)
+
+print(req.status_code)
+# print(req.json())
+print(req.text)
